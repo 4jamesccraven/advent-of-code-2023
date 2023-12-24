f5455bfd1dff0e2ae2f6e174c24ebad4c4c893b3
Christopher Craven <4jamesccraven@gmail.com>
2023-12-24 15:06:33 -0500

    Restarting day 2

--- 2/inc/gameInstance.h
+++ 2/inc/gameInstance.h
@@ -1,18 +1,2 @@
 #include<unordered_map>
 #include<string>
-
-struct validSet{
-    std::string colours[3];
-    int values[3];
-};
-
-class instance {
-public:
-    instance(validSet inCriteria, unsigned int inID);
-    void newPull(std::string colour, int count);
-    bool isValid();
-private:
-    unsigned int game_id_;
-    std::unordered_map<std::string, int> highest_possibility_;
-    validSet criteria_;
-};
--- 2/src/gameInstance.cpp
+++ 2/src/gameInstance.cpp
@@ -1,15 +1,1 @@
 #include <gameInstance.h>
-
-instance::instance(validSet inCriteria, unsigned int inID) : game_id_(inID), criteria_(inCriteria) {
-    for (unsigned int i = 0; i < 3; i++)
-        highest_possibility_[criteria_.colours[i]] = 0;
-}
-
-void instance::newPull(std::string colour, int count) {
-
-}
-
-bool instance::isValid() {
-
-    return false;
-}
