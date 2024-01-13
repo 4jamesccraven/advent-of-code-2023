0db28613e494fcc267e0f9eb0584a31ea2e2c926
Timothy Vandenbrande <timothy.vandenbrande@gmail.com>
2024-01-13 22:21:52 +0100

    flake8 approved

--- 3/Part 2/gearRatios.py
+++ 3/Part 2/gearRatios.py
@@ -1,19 +1,23 @@
 import sys
 
+
 class grid:
     def __init__(self, filename):
         with open(filename, "r") as f:
             self.internal_grid = [line.strip() for line in f]
         self.width = len(self.internal_grid[0])
         self.height = len(self.internal_grid)
-        self.stars = [[index for index, char in enumerate(line) if char == '*'] for line in self.internal_grid]
+        self.stars = [
+                [index for index, char in enumerate(line) if char == '*']
+                for line in self.internal_grid
+            ]
 
     def __getitem__(self, index):
         return self.internal_grid[index]
 
     def __repr__(self):
         return '\n'.join(self.internal_grid)
-    
+
     def locate_entire_number(self, x, y):
         num = self.internal_grid[y][x]
         if not num.isdigit():
@@ -39,13 +43,13 @@ class grid:
                     next = self.internal_grid[y][x + i]
                 else:
                     break
-        
+
         return int(num)
 
-    
-    def find_numbers_around_star(self, x , y):
-        left, right, top_one, top_two, bottom_one, bottom_two = 0, 0, 0, 0, 0, 0
-        
+    def find_numbers_around_star(self, x, y):
+        left, right, top_one, top_two, bottom_one, bottom_two = \
+            0, 0, 0, 0, 0, 0
+
         if x != 0 and self.internal_grid[y][x - 1].isdigit():
             left = self.locate_entire_number(x - 1, y)
 
@@ -58,7 +62,8 @@ class grid:
             else:
                 if x != 0 and self.internal_grid[y - 1][x - 1].isdigit():
                     top_one = self.locate_entire_number(x - 1, y - 1)
-                if x !=(self.height - 1) and self.internal_grid[y - 1][x + 1].isdigit():
+                if x != (self.height - 1) and \
+                        self.internal_grid[y - 1][x + 1].isdigit():
                     top_two = self.locate_entire_number(x + 1, y - 1)
 
         if y != (self.height - 1):
@@ -67,14 +72,15 @@ class grid:
             else:
                 if x != 0 and self.internal_grid[y + 1][x - 1].isdigit():
                     bottom_one = self.locate_entire_number(x - 1, y + 1)
-                if x != (self.height - 1) and self.internal_grid[y + 1][x + 1].isdigit():
+                if x != (self.height - 1) and \
+                        self.internal_grid[y + 1][x + 1].isdigit():
                     bottom_two = self.locate_entire_number(x + 1, y + 1)
 
         list = [left, right, top_one, top_two, bottom_one, bottom_two]
         list = [x for x in list if x != 0]
 
         return tuple(list)
-    
+
     def find_gear_ratios(self):
         numbers = []
         for idx, line in enumerate(self.stars):
@@ -85,9 +91,6 @@ class grid:
         print(len(numbers))
         return [num[0] * num[1] for num in numbers]
 
-        
-
-            
 
 def main():
     if len(sys.argv) != 2:
@@ -97,11 +100,12 @@ def main():
     print(f'Opening \'{sys.argv[1]}\'')
     schematic = grid(sys.argv[1])
 
-    #print(schematic)
+    # print(schematic)
 
     print(sum(schematic.find_gear_ratios()))
 
     return
+
 
 if __name__ == "__main__":
     main()
