b36c52da941b81e36c771b08059f2d0ce820cb48
4jamesccraven <4jamesccraven@gmail.com>
2025-10-20 22:35:30 -0400

    complete day 1 part 2

--- 1/calibrationCode.cpp
+++ 1/calibrationCode.cpp
@@ -1,10 +1,30 @@
-#include<iostream>
-#include<string>
-#include<vector>
-#include<sstream>
-#include<fstream>
+#include <algorithm>
+#include <cctype>
+#include <cstddef>
+#include <numeric>
+#include <print>
+#include <ranges>
+#include <array>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <fstream>
+
+const std::array<std::string, 9> ENGLISH_DIGITS {
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+};
 
 int parseLine(std::string line);
+int parsePermissive(const std::string &line);
 
 int main (int arc, char *argv[]) {
     //make sure there is at least one argument past the .exe
@@ -12,7 +32,7 @@ int main (int arc, char *argv[]) {
         std::cerr << "Invalid number of arguments; expected file name" << std::endl;
         return -1;
     }
-    
+
     std::ifstream fin;
     std::string filename = argv[1];
 
@@ -29,6 +49,7 @@ int main (int arc, char *argv[]) {
 
     //create a vector of numbers from lines for the final sum
     std::vector<int> numbersFromLines;
+    std::vector<int> numbersPermissive;
     while(!fin.eof()) {
         std::string line = "";
 
@@ -36,17 +57,19 @@ int main (int arc, char *argv[]) {
         std::getline(fin >> std::ws, line);
 
         //if the line isn't empty, parse it
-        if(!line.empty()) numbersFromLines.push_back(parseLine(line));
+        if(!line.empty()) {
+            numbersFromLines.push_back(parseLine(line));
+            numbersPermissive.push_back(parsePermissive(line));
+        }
     }
 
     //sum all the values
-    int total = 0;
-    for (auto num : numbersFromLines) {
-        total += num;
-    }
+    int p1 = std::accumulate(numbersFromLines.begin(), numbersFromLines.end(), 0);
+    int p2 = std::accumulate(numbersPermissive.begin(), numbersPermissive.end(), 0);
 
     //output result
-    std::cout << total << std::flush;
+    std::println("Part 1: {}", p1);
+    std::println("Part 2: {}", p2);
     return 0;
 }
 
@@ -73,7 +96,34 @@ int parseLine(std::string line) {
     std::string lastString = std::string(1, last);
 
     std::string output =  firstString + lastString;
-    
+
     //return the value only if the string isn't "  "
     return (first != ' ' && last !=' ') ? std::stoi(output) : 0;
+}
+
+int parsePermissive(const std::string &line) {
+    using pair_t = std::pair<std::size_t, char>;
+
+    auto candidates = std::views::iota(std::size_t{0}, line.size())
+        | std::views::transform([&line](std::size_t i) -> std::optional<pair_t> {
+              char c = line[i];
+
+              if (std::isdigit(c))
+                  return pair_t{i, c};
+
+              for (std::size_t d = 0; d < ENGLISH_DIGITS.size(); d++) {
+                  const auto &word = ENGLISH_DIGITS[d];
+                  if (line.compare(i, word.size(), word) == 0) {
+                      char digit_char = '1' + d;
+                      return pair_t{i, digit_char};
+                  }
+              }
+              return std::nullopt;
+          })
+        | std::views::filter([](const auto &opt) {return opt.has_value(); })
+        | std::views::transform([](const auto &opt) { return *opt; });
+
+    auto [min_pair, max_pair] = std::ranges::minmax(candidates, {}, &pair_t::first);
+
+    return ((min_pair.second - '0') * 10) + (max_pair.second - '0');
 }
